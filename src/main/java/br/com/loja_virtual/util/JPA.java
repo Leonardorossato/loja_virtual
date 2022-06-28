6d65bca73c5d6043a71d0283aa976dadb46a07fe
Leonardorossato <leonardorossato21@gmail.com>
2022-06-28 18:06:45 -0300

    utilidade para melhora com o jpa

---
+++ src/main/java/br/com/loja_virtual/util/JPA.java
@@ -0,0 +1,13 @@
+package br.com.loja.loja.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPA {
+    private static final EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("loja");
+
+    public static EntityManager getEntityManager(){
+        return FACTORY.createEntityManager();
+    }
+}
