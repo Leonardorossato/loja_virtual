2b31df30cba5c288217ad7241a88c47528d94fe2
Leonardorossato <leonardorossato21@gmail.com>
2022-06-28 18:06:00 -0300

    data access onjecr de produto

---
+++ src/main/java/br/com/loja_virtual/DAO/ProdutoDAO.java
@@ -0,0 +1,16 @@
+package br.com.loja_virtual.DAO;
+
+import javax.persistence.EntityManager;
+
+import br.com.loja.loja.model.Produto;
+
+public class ProdutoDAO {
+    private EntityManager em;
+
+    public ProdutoDAO(EntityManager em) {
+        this.em = em;
+    }
+    public void cadastrarProduto(Produto produto){
+        this.em.persist(produto);
+    }
+}
